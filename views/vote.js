f9f0fd6afbf6f83d96e4f186615911c7610bfb4e
Noah Freriksen <noah_freriksen@hotmail.com>
2021-11-24 13:15:52 +0100

    fullscreen

--- views/vote.js
+++ views/vote.js
@@ -1,3 +1,17 @@
+var docElm = document.documentElement;
+if (docElm.requestFullscreen) {
+    docElm.requestFullscreen();
+}
+else if (docElm.mozRequestFullScreen) {
+    docElm.mozRequestFullScreen();
+}
+else if (docElm.webkitRequestFullScreen) {
+    docElm.webkitRequestFullScreen();
+}
+else if (docElm.msRequestFullscreen) {
+    docElm.msRequestFullscreen();
+}
+
 function getNewFeatures()
 {
     fetch('/getFeatures').then(response => 
